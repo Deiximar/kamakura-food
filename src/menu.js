3542d0eccaa6fafd1d5e1fdd89cabfd821c08cdf
Deiximar Virriel <deiximar@gmail.com>
2024-06-04 18:53:38 +0200

    imprimir en pantalla los filtros de las categorías del menú con la información proporcionada en el archivo de data.js.

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,18 @@
 //DEBE imprimir en pantalla la información de filtros.
+import { filters, products } from '../assets/data/data.js'
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+
+const filtersContainer = document.querySelector('#filters');
+document.querySelector('.filter').remove();
+
+const createFilter = (filter) => {
+  const filterButton = document.createElement('button');
+  filterButton.classList = 'filter';
+
+  filterButton.innerHTML = filter;
+
+  filtersContainer.appendChild(filterButton);
+}
+
+filters.forEach(filter => createFilter(filter))
