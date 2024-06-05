8b863f52760c90b49eb9db162db8238ebfbf5304
tursdlc <ana89.sdlc@gmail.com>
2024-06-05 09:30:11 +0200

    boton carrito

--- src/events.js
+++ src/events.js
@@ -1,2 +1,11 @@
 //Intenta separar los eventos en este archivo.
 
+//id cart class cart-menu
+console.log("hola");
+
+document.getElementById("cart").addEventListener("click", displayCart);
+
+function displayCart() {
+    document.getElementById("cart-container");
+    //if else 
+}
