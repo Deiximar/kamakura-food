294810934678b37514c3a84e62e5204cdc9a2aa8
Deiximar Virriel <deiximar@gmail.com>
2024-06-05 17:02:46 +0200

    Agregar funcionalidad para filtrar platos segun su categoria

--- src/events.js
+++ src/events.js
@@ -1,2 +1,7 @@
 //Intenta separar los eventos en este archivo.
+import { filterProducts } from './menu.js'
+const addEventClickFilter = (button, filter) => {
+  button.addEventListener('click', () => filterProducts(filter))
+}
 
+export { addEventClickFilter }
