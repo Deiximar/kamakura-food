cf19301d6f3d057d6e01c0d305e34bebdf863160
paolaperdomo <paolaperdomo079@gmail.com>
2024-06-05 14:41:42 +0200

    Funcion de recibo creada

--- src/receipt.js
+++ src/receipt.js
@@ -1,1 +1,21 @@
-//Aquí intenta poner las funcionalidades del recibo
+
+const receiptProducts = document.querySelector(`#receipt-container`);
+document.querySelector(`.receipt-product`).remove();
+
+
+const createReceipt = (product) => {
+    const receiptProduct = document.createElement(`div`);
+    receiptProduct.classList = `receipt-product`;
+    receiptProduct.innerHTML = `  
+
+            <h3>${product.name}</h3>
+            <div class="receipt-price">
+                <p>Cantidad: ${product.amount}</p>
+                <h5>Subtotal ${product.subtotal}€</h5>
+            </div>     
+
+    `  
+    receiptProducts.insertBefore(receiptProduct,document.getElementById(`receipt-total`))
+}
+
+productSelected.forEach(producto => createReceipt(producto))
