083088a2190b9c56cc0b9b71250bd53225a3f716
tursdlc <ana89.sdlc@gmail.com>
2024-06-07 11:18:39 +0200

    arreglo funcionalidad cierre recibo de compra

--- src/receipt.js
+++ src/receipt.js
@@ -1,7 +1,7 @@
 
 const receiptProducts = document.querySelector(`#receipt-container`);
 document.querySelector(`.receipt-product`).remove();
-document.getElementById("close-receipt").addEventListener("click", closeReceipt);
+document.getElementById("close-receipt").addEventListener("click", ()=> closeReceipt());
 
 const createReceipt = (product) => {
     const receiptProduct = document.createElement(`div`);
