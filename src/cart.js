3cd5f6a58ee9ed6aa64dc168c3a4ff4f7e503b6d
Deiximar Virriel <deiximar@gmail.com>
2024-06-08 23:22:40 +0200

    Crear funcion que obtiene el cart en local storage

--- src/cart.js
+++ src/cart.js
@@ -1,95 +1,105 @@
 //DEBE contener las funcionalidades del carrito de compras.
-
 import { products } from "../assets/data/data.js";
-const cart = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
 
 const cartProducts = document.querySelector("#cart-products");
 document.querySelector(".cart-container").remove();
 
-// Agrega un event listener al contenedor de productos
-function createProductInCart() {
+function loadAllProducts() {
   document.querySelectorAll(".cart-container").forEach(product => product.remove());
-  cart.map((item) => {
-    let product = searchProduct(item.id);
-    const cartContainer = document.createElement("div");
-    cartContainer.classList = "cart-container";
+  const cart = getCart();
+  cart.forEach(product => createProductInCart(product));
+}
 
-    const addbutton = document.createElement('button');
-    addbutton.textContent = '+';
-    addbutton.addEventListener('click', () => addProductAmount(product.id));
+// Agrega un event listener al contenedor de productos
+function createProductInCart(cartProduct) {
+  const product = searchProduct(cartProduct.id)
+  const cartContainer = document.createElement("div");
+  cartContainer.classList = "cart-container";
 
-    const substractButton = document.createElement('button');
-substractButton.textContent = '-';
-substractButton.addEventListener('click', () => substractProductAmount(product.id));
+  const addbutton = document.createElement('button');
+  addbutton.textContent = '+';
+  addbutton.addEventListener('click', () => addProductAmount(product.id));
 
-    cartContainer.classList = "cart-container";
-    cartContainer.innerHTML = `
+  const substractButton = document.createElement('button');
+  substractButton.textContent = '-';
+  substractButton.addEventListener('click', () => substractProductAmount(product.id));
+
+  cartContainer.classList = "cart-container";
+  cartContainer.innerHTML = `
         <button class="close-button"><img src="./assets/img/close.svg" alt="close"></button>
         <div class="text-container">
             <h3>${product.name}</h3>
             <h5>${product.price}€</h5>
         </div>
         <div class="quantity-container" id="quantity-${product.id}">
-            <p class="quantity">${item.item}</p>
+            <p class="quantity">${cartProduct.quantity}</p>
 
         </div>
-`;
-    cartProducts.appendChild(cartContainer);
-    const quantityContainer = cartContainer.querySelector(`#quantity-${product.id}`);
-    quantityContainer.insertBefore(addbutton, quantityContainer.querySelector('.quantity'))
-    quantityContainer.appendChild(substractButton);
-  })
+    `;
+  cartProducts.appendChild(cartContainer);
+
+  const quantityContainer = cartContainer.querySelector(`#quantity-${product.id}`);
+  quantityContainer.insertBefore(addbutton, quantityContainer.querySelector('.quantity'))
+  quantityContainer.appendChild(substractButton);
 }
 
+const getCart = () => {
+  return localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+}
 
-const addProduct = (id) => {
-  const search = cart.find(item => item.id == id);
-  if (search === undefined) {
-    cart.push({
+const addCartProduct = (id) => {
+  const cart = getCart();
+  let cartProduct = cart.find(item => item.id == id);
+  if (cartProduct === undefined) {
+    cartProduct = {
       id: id,
-      item: 1,
-    });
-    createProductInCart();
+      quantity: 1,
+    };
+    cart.push(cartProduct);
+    createProductInCart(cartProduct);
   } else {
-    search.item += 1;
-    updateQuantityText(search);
+    cartProduct.quantity += 1;
   }
   localStorage.setItem("cart", JSON.stringify(cart));
+  updateQuantityText(cartProduct);
 }
-
 
 const searchProduct = (id) => {
   return products.find((product) => product.id == id) || [];
 }
 
 const substractProductAmount = (id) => {
+  const cart = getCart();
   const findProduct = cart.find(product => product.id == id);
-  
-    if (findProduct.item > 1) {
-      findProduct.item -= 1;
-      updateQuantityText(findProduct);
-    } else {
-      const index = cart.indexOf(findProduct);
-      if (index > -1) {
-        cart.splice(index, 1);
-      }
-      createProductInCart();
+
+  if (findProduct.quantity > 1) {
+    findProduct.quantity -= 1;
+    updateQuantityText(findProduct);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  } else {
+    const index = cart.indexOf(findProduct);
+    if (index > -1) {
+      cart.splice(index, 1);
     }
     localStorage.setItem("cart", JSON.stringify(cart));
+    loadAllProducts();
+  }
 }
 
 const addProductAmount = (id) => {
+  const cart = getCart();
   const findProduct = cart.find(product => product.id == id)
-  findProduct.item += 1;
+  findProduct.quantity += 1;
   updateQuantityText(findProduct);
   localStorage.setItem("cart", JSON.stringify(cart))
 }
 
 const updateQuantityText = (cartProduct) => {
-  document.querySelector(`#quantity-${cartProduct.id}`).querySelector('.quantity').textContent = cartProduct.item;
+  const element = document.querySelector(`#quantity-${cartProduct.id} .quantity`);
+  element.textContent = cartProduct.quantity;
 }
 
 
-createProductInCart();
-export { addProduct, addProductAmount, substractProductAmount }
+loadAllProducts();
+export { addCartProduct, addProductAmount, substractProductAmount, getCart }
 
