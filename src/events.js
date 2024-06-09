eee8a0c5a57d083949a7152729cbb7a15b41e7ae
Deiximar Virriel <deiximar@gmail.com>
2024-06-10 01:22:57 +0200

    ocultar productos en el recibo cuando se hace click en el boton proceder al pago

--- src/events.js
+++ src/events.js
@@ -21,11 +21,9 @@ const addEventClickFilter = (button, filter) => {
 
 function displayReceipt() {
   const receiptContainer = document.getElementById("receipt-container");
-  if (receiptContainer.style.display === "none") {
-    receiptContainer.style.display = "flex";
-  } else {
-    receiptContainer.style.display = "none"
-  }
+  const productsContainer = document.getElementById("products-container");
+  receiptContainer.style.display = "flex";
+  productsContainer.style.display = "none";
 }
 
 const text = document.querySelector('#cart-products > h3');
