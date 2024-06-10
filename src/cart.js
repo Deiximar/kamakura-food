90b9571c7289ac76a948d4346639890f78a877e5
Deiximar Virriel <deiximar@gmail.com>
2024-06-10 09:38:54 +0200

    Elimnar todo el plato en el carrito cuando se hace click en el boton X

--- src/cart.js
+++ src/cart.js
@@ -1,6 +1,6 @@
 //DEBE contener las funcionalidades del carrito de compras.
 import { products } from "../assets/data/data.js";
-import { hideText } from "./events.js";
+import { hideText, removeProductEvent } from "./events.js";
 
 const cartProducts = document.querySelector("#cart-products");
 document.querySelector(".cart-container").remove();
@@ -39,7 +39,8 @@ function createProductInCart(cartProduct) {
         </div>
     `;
   cartProducts.appendChild(cartContainer);
-
+  const closeButton = document.querySelector(`#cart-product-${cartProduct.id} > .close-button`);
+  removeProductEvent(closeButton, `${cartProduct.id}`);
   const quantityContainer = cartContainer.querySelector(`#quantity-${product.id}`);
   quantityContainer.insertBefore(addbutton, quantityContainer.querySelector('.quantity'))
   quantityContainer.appendChild(substractButton);
@@ -131,6 +132,16 @@ const updateTotal = () => {
   totalElement.textContent = `Total: ${total}€`
 }
 
+function removeProduct(productID) {
+  const cart = getCart();
+  console.log(productID)
+  console.log(cart);
+  const newCart = cart.filter((cartProduct) => cartProduct.id != productID);
+  console.log(newCart, 'new');
+  localStorage.setItem("cart", JSON.stringify(newCart));
+  updateTotal();
+}
+
 loadAllProducts();
-export { addCartProduct, addProductAmount, substractProductAmount, getCart }
+export { addCartProduct, addProductAmount, substractProductAmount, getCart, removeProduct }
 
